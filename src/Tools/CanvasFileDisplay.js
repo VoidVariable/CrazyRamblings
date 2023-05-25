b2319424880fdf2869a021e3514e252fcb9fd1a9
VoidVariable <thevoidvariable@gmail.com>
2023-05-25 22:03:24 +0100

    Test canvas warnings

--- src/Tools/CanvasFileDisplay.js
+++ src/Tools/CanvasFileDisplay.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import "./CanvasFileDisplay.css";
+import fetchingData from './dataFetcher';
 
 class CanvasFileDisplay extends React.Component {
-  render() {
-    const { path } = this.props;
+    constructor(props) {
+    super(props);
+    this.state = {
+        canvasData: null,
+        error: null
+    };
+    }
+    
+    componentDidMount() {
+    this.fetchImageData();
+    }
 
+    componentDidUpdate(prevProps) {
+    if (prevProps.path !== this.props.path) {
+        this.fetchImageData();
+    }
+    }
+
+    fetchImageData = async () => {
+        try {
+          const markdownData = await fetchingData('text', this.props.path);
+    
+          this.setState({ canvasData: markdownData });
+          // Do something with the markdownData, such as displaying it in the component state or rendering it.
+        } catch (error) {
+          // Handle any errors that occur during the fetch.
+          console.error('Error fetching Markdown data:', error);
+        }
+      };
+
+    render() {
+    const { path } = this.props;
+     
     return (
         <div className="canvas-file-display">
         <label>Path: {path}</label>
-        <div className='canvas-warning'>File type not Implement</div>
+        <div className='canvas-warning'>File type not Implement {this.state.canvasData}</div>
       </div>
     );
   }
