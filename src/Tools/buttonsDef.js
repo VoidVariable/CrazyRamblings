50f1743d536e151a2b273e60feef09b2f8f29aa8
VoidVariable <thevoidvariable@gmail.com>
2023-06-05 20:33:49 +0100

    Fix path

--- src/Tools/buttonsDef.js
+++ src/Tools/buttonsDef.js
@@ -1,6 +1,6 @@
 // Array of vault button data
 export const vaultButtons = [
-    { id: 1, label: 'Deltarune', link: "https://raw.githubusercontent.com/VoidVariable/CrazyRamblings/main/src/Tools", name: 'Obsidian' },
+    { id: 1, label: 'Deltarune', link: "https://raw.githubusercontent.com/VoidVariable/CrazyRamblings/main/src/Tools", name: 'Deltarune' },
     { id: 2, label: 'Undertale', link: "https://raw.githubusercontent.com/VoidVariable/CrazyRamblings/main/src/Tools" , name: 'Undertale' },
     { id: 3, label: 'Other Game IDK', llink: "https://raw.githubusercontent.com/VoidVariable/CrazyRamblings/main/src/Tools", name: 'Test' }
     // Add more vault buttons as needed
