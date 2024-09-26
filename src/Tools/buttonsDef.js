bed2006ec2c4af79fb321da748844c4428a01dd0
dtfernandes <danielteotonio@sapo.pt>
2024-09-26 13:56:32 +0100

    Test build

--- src/Tools/buttonsDef.js
+++ src/Tools/buttonsDef.js
@@ -1,19 +1,19 @@
 // Array of vault button data
 export const vaultButtons = [
-    { id: 1, label: 'Deltarune', link: "https://raw.githubusercontent.com/VoidVariable/CrazyRamblings/main/src/Tools", name: 'Deltarune' },
-    { id: 2, label: 'Undertale', link: "https://raw.githubusercontent.com/VoidVariable/CrazyRamblings/main/src/Tools" , name: 'Undertale' },
-    { id: 3, label: 'Other Game IDK', llink: "https://raw.githubusercontent.com/VoidVariable/CrazyRamblings/main/src/Tools", name: 'Test' }
-    // Add more vault buttons as needed
-  ];
+  { id: 1, label: 'Deltarune', link: "https://raw.githubusercontent.com/VoidVariable/CrazyRamblings/main/src/Tools", name: 'Deltarune' },
+  { id: 2, label: 'Undertale', link: "https://raw.githubusercontent.com/VoidVariable/CrazyRamblings/main/src/Tools", name: 'Undertale' },
+  { id: 3, label: 'Other Game IDK', llink: "https://raw.githubusercontent.com/VoidVariable/CrazyRamblings/main/src/Tools", name: 'Test' },
+  { id: 4, label: 'Other one', llink: "https://raw.githubusercontent.com/VoidVariable/CrazyRamblings/main/src/Tools", name: 'Test' }
+  // Add more vault buttons as needed
+];
 
 
-  export function getSingleVaultByLabel(label) {
-    const selectedVault = vaultButtons.find(button => button.name === label);
-  
-    if (selectedVault) {
-      return selectedVault;
-    }
-  
-    return null;
+export function getSingleVaultByLabel(label) {
+  const selectedVault = vaultButtons.find(button => button.name === label);
+
+  if (selectedVault) {
+    return selectedVault;
   }
-  
+
+  return null;
+}
